6222c5817d7fd4268e9bd08d1b2f448b461f21d6
Nejc Šneberger <62680385+snebi17@users.noreply.github.com>
2021-08-21 12:08:58 +0200

    Revert "Even newer design"

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,26 @@
 <template>
 	<v-app>
-		<v-navigation-drawer
-			v-model="drawer"
-			absolute
-			temporary
-			app
-			color="secondary"
-		>
-			<v-list>
-				<v-list-item 
-					link
-					:to="{ path: '/menu' }"
-					class="success--text"
-				>
-					Menu
-				</v-list-item>
-				<v-list-item 
-					link
-					:to="{ path: '/gallery' }"
-					class="success--text"
-				>
-					Galerija
-				</v-list-item>
-				<v-list-item 
-					link
-					:to="{ path: '/about' }"
-					class="success--text"
-				>
-					O nas
-				</v-list-item>
-				<v-list-item 
-					link
-					:to="{ path: '/contact' }"
-					class="success--text"
-				>
-					Delovni čas in kontakt
-				</v-list-item>
-			</v-list>
-		</v-navigation-drawer>
 		<v-app-bar 
 			flat 
 			color="transparent"
-			fixed
 			app
+			fixed
 		>
-			<v-app-bar-nav-icon 
-				x-large 
-				color="secondary"
-				@click="drawer = true"
-				icon
-			></v-app-bar-nav-icon>
-			<v-spacer></v-spacer>
 			<v-btn 
 				x-large 
 				color="secondary"
 				icon
 				@click="open('https://www.facebook.com/LaCreperieCheriPalacinke/')"
 			>
-				<i class="fa fa-facebook fa-lg"></i>
+				<v-icon>mdi-facebook</v-icon>
 			</v-btn>
 			<v-btn 
-				x-large
+				x-large 
 				color="secondary"
 				icon
 				@click="open('https://www.instagram.com/lacreperie_cheri/')"
 			>
-				<i class="fa fa-instagram fa-lg"></i>
+				<v-icon>mdi-instagram</v-icon>
 			</v-btn>
 			<v-btn 
 				x-large 
@@ -73,13 +28,50 @@
 				icon
 				@click="open('https://www.tripadvisor.com/Restaurant_Review-g274873-d23450735-Reviews-La_Creperie_Cheri-Ljubljana_Upper_Carniola_Region.html')"
 			>
-				<i class="fa fa-tripadvisor fa-lg"></i>
+				<v-icon>mdi-</v-icon>
 			</v-btn>
+			<v-spacer></v-spacer>
+			<v-app-bar-nav-icon 
+				x-large 
+				color="secondary"
+				@click="drawer = true"
+			></v-app-bar-nav-icon>
 		</v-app-bar>
-		<v-content 
-			class="mt-n14 mt-md-n16"
+		<v-navigation-drawer
+			v-model="drawer"
+			absolute
+			temporary
+			right
 			app
 		>
+			<v-list dense>
+				<v-list-item 
+					link
+					:to="{ path: '/menu' }"
+				>
+					Menu
+				</v-list-item>
+				<v-list-item 
+					link
+					:to="{ path: '/gallery' }"
+				>
+					Gallery
+				</v-list-item>
+				<v-list-item 
+					link
+					:to="{ path: '/about' }"
+				>
+					O nas
+				</v-list-item>
+				<v-list-item 
+					link
+					:to="{ path: '/contact' }"
+				>
+					Delovni čas in kontakt
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+		<v-content class="mt-n16">
 			<v-layout fill-height>
 				<router-view></router-view>
 			</v-layout>
