1591a0461a5ef9d96a094d19129bdf7d13ff451b
Nejc Šneberger <nejc.snebi@gmail.com>
2021-08-28 12:04:33 +0200

    commit

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container fluid fill-height class="v-container__custom">
-        <v-row align="center">                
+        <v-row align="center v-row__opacity">                
             <v-col class="text-center">
                 <p class="text-h5 text-md-h3 font-weight-bold secondary--text">La Crêperie Chéri</p>
             </v-col>
@@ -18,5 +18,12 @@ export default {
     .v-container__custom {
         background: url('../assets/laPistacchio2.jpg')!important;
         background-size: cover!important;
+        background-color: rgba(0, 0, 0, 1);
+    }
+    .v-row__opacity {
+        background-color: rgba(0, 0, 0, .6);
+        position: absolute;
+        width: 100%;
+        height: 100%;
     }
 </style>
